2fd438c1e3bc16875f45f8307eff252d418efa72
Olga Cygankowa <olga.cygankowa@gmail.com>
2024-03-01 14:37:21 +0100

    Fix post card height

--- template-parts/content-fundraising-card.php
+++ template-parts/content-fundraising-card.php
@@ -1,9 +1,9 @@
 <div class="col mb-30">
-    <div class="service-item">
+    <div class="service-item h-100 d-flex flex-column">
         <div class="ser-img mb-25"><img src="<?php echo get_field( 'img_card' )['sizes']['450x600']; ?>"
                                         alt="<?php echo get_field( 'img_card' )['alt']; ?>">
         </div>
-        <div class="description">
+        <div class="description h-100">
             <h6><a href="<?php the_permalink(); ?>"><?php the_field( 'title_banner' ); ?></a></h6>
             <p><?php the_field( 'description' ); ?></p>
             <a href="<?php the_permalink(); ?>"
