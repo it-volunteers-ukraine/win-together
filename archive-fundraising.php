b19fda77525c469c6a9f3e84bf05620435affea4
Olga Cygankowa <olga.cygankowa@gmail.com>
2024-03-01 14:26:34 +0100

    Add post pagination

--- archive-fundraising.php
+++ archive-fundraising.php
@@ -18,11 +18,14 @@
     <div class="services-area pt-80 pb-50">
         <div class="container">
             <div class="row row-cols-lg-3 row-cols-md-2 row-cols-sm-1 row-cols-1">
-				<?php $args = ( [
-					'posts_per_page' => - 1,
+				<?php $args       = ( [
+//					'posts_per_page' => 6,  // page size set with WP reading setting
 					'post_type'      => 'fundraising',
+					'paged'          => get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1,
 				] );
-					$query  = new WP_Query( $args );
+					$query        = new WP_Query( $args );
+					$total_pages  = $query->max_num_pages;
+					$current_page = max( 1, get_query_var( 'paged' ) );
 					if ( $query->have_posts() ) :
 						while ( $query->have_posts() ) :
 							$query->the_post(); ?>
@@ -32,6 +35,22 @@
 						<?php endwhile;
 						wp_reset_postdata();
 					endif; ?>
+            </div>
+
+            <div class="custom-pagination d-flex justify-content-center">
+				<?php
+					echo paginate_links( [
+						'base'      => get_pagenum_link( 1 ) . '%_%',
+						'format'    => '/page/%#%',
+						'current'   => $current_page,
+						'total'     => $total_pages,
+						'prev_text'          => '<',
+						'next_text'          => '>',
+						'show_all'  => $total_pages <= 5,
+						'end_size'  => 1,
+						'mid_size'  => ( $current_page === 1 ) || ( $current_page == $total_pages ) ? 3 : 1,
+					] );
+				?>
             </div>
         </div>
     </div>
