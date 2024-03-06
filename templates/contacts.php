009e65bdb5af7e1c2f2ab29bbae0a9e90dab2337
Olga Cygankowa <olga.cygankowa@gmail.com>
2024-03-06 20:23:06 +0100

    Add conditional render for email and phone at contacts

--- templates/contacts.php
+++ templates/contacts.php
@@ -22,7 +22,6 @@
 <?php endif; ?>
     <!-- ================ Banner area end ================ -->
 
-
     <!-- ================ Contact area ================ -->
     <div class="contact-page contact-area pb-50 pt-60">
         <div class="container">
@@ -30,66 +29,12 @@
                 <h2><?php the_field( 'title' ); ?></h2>
                 <span class="border-title"></span>
             </div>
-
             <p class="description mx-auto text-center mb-md-5"><?php the_field( 'text' ); ?></p>
-
-            <div class="row contact-row pt-md-3">
-                <div class="col-lg-7 col-md-5 mb-30">
-                    <!-- map box -->
-                    <div class="map-box">
-                        <iframe src="<?php the_field( 'google_map', 'option' ); ?>"></iframe>
-                    </div>
-                    <!-- map box end -->
-                </div>
-                <div class="col-lg-5 col-md-7 mb-30">
-                    <!-- contact info -->
-                    <div class="contact-info-box h-100">
-                        <h3 class="text-white mb-20 "><?php the_field( 'card_title' ); ?></h3>
-                        <ul class="contact-info">
-                            <li><a href="tel:<?php the_field( 'phone', 'option' ); ?>">
-                                    <i class="fas fa-phone-alt"></i> <?php the_field( 'phone_display', 'option' ); ?>
-                                </a>
-                            </li>
-                            <li><a href="mailto:<?php the_field( 'email', 'option' ); ?>">
-                                    <i class="far fa-envelope"></i> <?php the_field( 'email', 'option' ); ?></a>
-                            </li>
-                            <li><i class="fas fa-map-marker-alt"></i> <?php the_field( 'address', 'option' ); ?></li>
-                        </ul>
-                        <!-- contact social -->
-                        <div class="contact-social mt-20">
-							<?php if ( get_field( 'facebook', 'option' ) ) { ?>
-                                <a href="<?php the_field( 'facebook', 'option' ); ?>"
-                                   target="_blank" aria-label="facebook" class="me-2 mt-2">
-                                    <i class="fab fa-facebook-f"></i>
-                                </a>
-							<?php } ?>
-
-							<?php if ( get_field( 'instagram', 'option' ) ) { ?>
-                                <a href="<?php the_field( 'instagram', 'option' ); ?>"
-                                   target="_blank" aria-label="Instagram" class="me-2 mt-2">
-                                    <i class="fab fa-instagram"></i>
-                                </a>
-							<?php } ?>
-
-							<?php if ( get_field( 'twitter', 'option' ) ) { ?>
-                                <a href="<?php the_field( 'twitter', 'option' ); ?>"
-                                   target="_blank" aria-label="twitter" class="me-2 mt-2">
-                                    <i class="fab fa-twitter"></i>
-                                </a>
-							<?php } ?>
-							<?php if ( get_field( 'telegram', 'option' ) ) { ?>
-                                <a href="<?php the_field( 'telegram', 'option' ); ?>"
-                                   target="_blank" aria-label="telegram">
-                                    <i class="fab fa-telegram-plane"></i>
-                                </a>
-							<?php } ?>
-                        </div>
-                        <!-- contact social end -->
-                    </div>
-                    <!-- contact info end -->
-                </div>
-            </div>
         </div>
+
+		<?php get_template_part( 'template-parts/content', 'contact-section', array(
+			'title' => get_field( "card_title" )
+		) ); ?>
     </div>
     <!-- ================ Contact area end ================ -->
 
